c2882ddee73a9d9313dea12d62b98151c64d09c8
Dipil Jain <dipjain2@publicisgroupe.net>
2018-11-29 12:05:29 +0530

    Added Square Servlet

---
+++ src/com/mydemo/demoapp/AddServlet.java
@@ -0,0 +1,56 @@
+package com.mydemo.demoapp;
+
+import java.awt.image.RescaleOp;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+			int i = Integer.parseInt(req.getParameter("num1"));
+			int j = Integer.parseInt(req.getParameter("num2"));
+			
+			int result = i+j;
+			System.out.println("Sum is "+result);
+			
+			try {
+				res.getWriter().println("Sum is "+result);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			};
+			
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = i+j;
+		System.out.println("Sum is "+result);
+		
+		try {
+			res.getWriter().println("Sum is "+result);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		req.setAttribute("sum", result);
+		rd.forward(req, res);
+		
+}
+}
---
+++ src/com/mydemo/demoapp/SquareServlet.java
@@ -0,0 +1,41 @@
+package com.mydemo.demoapp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SquareServlet
+ */
+@WebServlet("/SquareServlet")
+public class SquareServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SquareServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append(" and the Square root of the sum of these two numbers is "+((int)request.getAttribute("sum")*(int)request.getAttribute("sum")));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
